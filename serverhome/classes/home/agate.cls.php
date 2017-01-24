14d55c7e8c61c28fa81f63b917d7c5f9be36bc08
ghooost <ghooost@gmail.com>
2017-01-24 17:46:24 +0300

    Home+roles

--- serverhome/classes/home/agate.cls.php
+++ serverhome/classes/home/agate.cls.php
@@ -1,9 +1,12 @@
 <?php
 class Home {
+    const DEF_ROLES_OUT_ACTION='SET_APP_ROLES';
     public function doAction($in){
       $action=empty($in['action'])?'':$in['action'];
       switch ($action){
         default:
+          $session=AppData::getItem('session');
+          AppData::setOutput(self::DEF_ROLES_OUT_ACTION,'roles',$session->getRoles());
           AppData::setOutput("SET_APP_MODULE","cls","Home");
       }
     }
